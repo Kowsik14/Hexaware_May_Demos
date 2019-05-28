e14897546e3e422c94551263796f8362b06f5fd1
Syed <syed.khaleel@outlook.com>
2019-05-28 16:17:03 +0530

    Day2_CSharp

---
+++ Helloworld.cs
@@ -0,0 +1,13 @@
+using System;
+namespace HexaWare
+{
+class HelloWorld
+{
+	public static void Main()
+	{
+	     Console.WriteLine("Hello world");
+	     Console.WriteLine("Hello Hexaware");
+	     //Console.WriteLine(args.Length);
+	}
+}
+}
